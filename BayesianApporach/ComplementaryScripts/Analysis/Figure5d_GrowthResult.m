c04fbc07f134d98ac0f955efeddcfc7dcdf9acf0
feiranl <feiranl@chalmers.se>
2021-05-20 17:24:02 +0200

    feat-update all figure analysis function

---
+++ BayesianApporach/ComplementaryScripts/Analysis/Figure5d_GrowthResult.m
@@ -0,0 +1,54 @@
+% Figure 5d
+% This function is to plot the figure5d, which is mainly focusing on
+% evalulation of the kcat tuning process
+% Running this function requires that all tuned emodels are genrated
+
+[~,~,growthrates] = xlsread('growthratedata.xlsx','growthrates');
+growthrates = growthrates(2:end,:);
+spcies = unique(growthrates(:,4));
+species = strrep(species,' ','_');
+
+
+% Figure 3A this plot is to show the growth rate prediction vs experimental
+% values
+result_constrain = [];
+result_max = [];
+
+for i = 1:length(species)
+    cd(species{i})% later change to cd(species{i}) use species id to name the folder
+    load([species{i},'sim_phen.mat']) % is generated in the function simulategrowth.m
+    result_constrain = [result_constrain;growthdata(:,[1:3,14]),num2cell(simulated_meadian(1:length(growthdata(:,1)),1))];
+    result_max = [result_max;max_growth(:,[1:3,14]),num2cell(simulated_meadian(length(growthdata(:,1))+1:end,1))];
+    cd ../
+end
+
+% datapoint
+all_data = [result_constrain;result_max];
+dp = cell2mat(all_data(:,[3,5]));
+
+sub = unique(all_data(:,2));
+color = [228,26,28;55,126,184;77,175,74;152,78,163;255,127,0;255,255,51;166,86,40;247,129,191;153,153,153;2,2,2]./255;
+
+hold on
+for j = 1:length(sub)
+idx = find(strcmpi(all_data(:,2),sub(j)) & strcmpi(all_data(:,4),'aerobic'));
+plot(dp(idx,1),dp(idx,2),'o','LineWidth',0.75,'MarkerSize',5);
+end
+for j = 1:length(sub)
+idx = find(strcmpi(all_data(:,2),sub(j)) & strcmpi(all_data(:,4),'anaerobic'));
+plot(dp(idx,1),dp(idx,2),'<','LineWidth',0.75,'MarkerSize',5);
+end
+set(gca,'FontSize',6,'FontName','Helvetica');
+legend(sub,'Fontsize',6)
+limit = max(dp(:));
+xlim([0 limit]);
+ylim([0 limit]);
+x = [0:0.1:limit];
+plot(x,x,'--k','LineWidth',0.75)
+
+ylabel('Simulated growth rate [1/h]','FontSize',7,'FontName','Helvetica','Color','k');
+xlabel('Experimental growth rate [1/h]','FontSize',7,'FontName','Helvetica','Color','k');
+
+set(gcf,'position',[500 200 180 130]);
+set(gca,'position',[0.27 0.33 0.7 0.63]);
+
---
+++ BayesianApporach/ComplementaryScripts/Analysis/Figure5e_SimulateProteomeSimplied.m
@@ -0,0 +1,129 @@
+% This analysis is to test whether the model proteome is similar to the
+% measured proteome
+current_path = pwd;
+% load proteome data
+[~,~,raw_proteome] = xlsread('../../ComplementaryData/proteome/Proteome_ref_new.xlsx','alldata');
+raw_proteome = raw_proteome(2:end,:); % the first is the head line
+cond = unique(raw_proteome(1,:),'stable');
+lable  = unique(raw_proteome(2,:),'stable');
+speciesid = {'Sce','Kma','Kla','Yli'};
+species = {'Saccharomyces_cerevisiae','Kluyveromyces_marxianus','Kluyveromyces_lactis','Yarrowia_lipolytica'};
+%inputpath = {'../KcatTuning/yeast8_witheclimit_abcmax_clean_more_var1','../KcatTuning/kmx_var1','../KcatTuning/kla_var1','../KcatTuning/yli_var1'};
+
+states = {'median','auto','prior','posterior'};
+
+
+for i = 1:length(cond)
+    for k = 1:4
+        k
+        state = states{k};
+        disp([num2str(i),'/',num2str(length(cond))])
+        %subplot(4,6,i)
+        code = cond{i};
+        code = split(code,'_');
+        [~,idx] = ismember(code(1),speciesid);
+        code(end+1) = species(idx);
+        code(end+1) = {['../KcatTuning/model_bayesian/',code{7}]};
+        % get flux
+        [model,sol_result_mean] = getflux(code{7},code{2},code{3},code{4},code{5},code{8},state); % species chemostat/batch media o2 carbon inputpath
+        sol_result{i,k} = sol_result_mean;
+        models{i} = model;
+        % get proteome
+        idx = ismember(raw_proteome(1,:),cond(i));
+        raw_proteome_tmp = raw_proteome(4:end,idx);
+        if strcmp(code{1},'Sce') || strcmp(code{6},'protein')
+            proteome.gene = raw_proteome_tmp(:,1); % geneID
+            if strcmp(code{6},'protein')
+                proteome.gene = convert2panID(proteome.gene); % change to S.cereviase gene
+            end
+        else
+            proteome.gene = raw_proteome_tmp(:,2); % geneID in the model species@seq1
+        end
+        proteome.abun = cell2mat(raw_proteome_tmp(:,end)); % protein abundance in mmol
+        proteome.abun(~cell2mat(cellfun(@ischar,proteome.gene,'UniformOutput',false))) = []; % delete empty line
+        proteome.gene(~cell2mat(cellfun(@ischar,proteome.gene,'UniformOutput',false))) = []; % delete empty line
+        proteome.gene(proteome.abun == 0|isnan(proteome.abun)) = []; % delete 0 abundance protein
+        proteome.abun(proteome.abun == 0|isnan(proteome.abun)) = []; % delete 0 abundance protein
+        if strcmp(code{1},'Sce') || strcmp(code{6},'protein')
+            [~,idx] = ismember(model.proteins,proteome.gene);
+        else
+            [~,idx] = ismember(model.genes,proteome.gene);
+        end
+        proteome_abun = zeros(length(model.genes),1);
+        proteome_abun(idx~=0) = proteome.abun(idx(idx~=0));
+        proteome_abun = proteome_abun.*model.MWs;
+        enzUsage_abun = calculateproteome(model,sol_result{i,k},model.genes);
+        
+        
+        abun(:,1) = proteome_abun;
+        gene(:,1) = model.proteins;
+        abun(:,k+1) = enzUsage_abun;
+        clear proteome_abun enzUsage_abun
+    end
+    
+    % get pan abundance by sum all paralog abundance
+    protein = unique(model.proteins);
+    allresult.gene(1:length(protein),1,i) = protein;
+    for j = 1:length(protein)
+        idx = ismember(model.proteins,protein(j));
+        allresult.abun(j,:,i) = sum(abun(idx,:),1);
+    end
+    clear abun gene   
+    
+    
+    % find congene and calculate the rmse
+    tmp_result =  allresult.abun(:,:,i);
+    tmp_result(tmp_result < 1E-6) = 0; % lower than the tolerance
+    tmp_result_gene =  allresult.gene(:,:,i);
+    tmp_result_gene(any(tmp_result==0,2),:) = [];
+    tmp_result(any(tmp_result==0,2),:) = [];
+    
+    for m = 1:length(states)
+        % plot for corr
+        figure
+        [RHO4,PVAL4] = corr(log10(tmp_result(:,1)),log10(tmp_result(:,1+m)),'Type','Pearson');
+        scatter(log10(tmp_result(:,1)),log10(tmp_result(:,1+m)),10,'o','filled','LineWidth',1,'MarkerEdgeColor',[49,130,189]/255,'MarkerFaceColor',[49,130,189]/255,'MarkerFaceAlpha',0.5);
+        text(median(log10(tmp_result(:,1))),median(log10(tmp_result(:,1+m))),['r = ' num2str(round(RHO4,3))],'FontSize',6,'FontName','Helvetica');
+        text(median(log10(tmp_result(:,1))),median(log10(tmp_result(:,1+m))-1),['p = ' num2str(round(PVAL4,10))],'FontSize',6,'FontName','Helvetica');
+        title([strrep(cond{i},'_','\_'),' (n = ',num2str(length(tmp_result_gene)),')'],'FontSize',8,'FontName','Helvetica');
+        result.pearsonr(i,m) = RHO4;
+        result.pvalue(i,m) = PVAL4;
+        result.rmse(i,m) = sqrt(immse(log10(tmp_result(:,1)),log10(tmp_result(:,1+m))));
+        result.genenum(i,m) = length(tmp_result_gene);
+    end
+end
+
+% figure 
+colorveryhigh = [254,235,226]/255;
+colorhigh = [251,180,185]/255;
+colormedium = [247,104,161]/255;
+colorlow = [174,1,126]/255;
+figure();
+b = bar(1:22,result.rmse');
+b(1).LineWidth = 0.5;
+b(1).FaceColor = colorveryhigh;
+b(2).LineWidth = 0.5;
+b(2).FaceColor = colorhigh;
+b(3).LineWidth = 0.5;
+b(3).FaceColor = colormedium;
+b(4).LineWidth = 0.5;
+b(4).FaceColor = colorlow;
+set(gca,'XTick',1:1:22);
+set(gca,'XTickLabel',strrep(lable,'_','\_'));
+xtickangle(90)
+ylim([0 2.3]);
+leg = legend({'global kcat' 'Auto' 'DL' 'Bayesian'},'Location','northwest','Orientation','horizontal','FontSize',6,'FontName','Helvetica');
+leg.ItemTokenSize = [10,2];
+set(leg,'box','off');
+set(gca,'FontSize',6,'FontName','Helvetica');
+ylabel('RMSE for proteome prediction','FontSize',7,'FontName','Helvetica','Color','k');
+set(gcf,'position',[200 100 300 150]);
+set(gca,'position',[0.1 0.2 0.75 0.5]);
+% remove the tick from right axis and top axis
+box off;
+ax1 = gca;
+ax2 = axes('Position', get(ax1, 'Position'), 'FontSize', 6,...
+           'Color','None','XColor','k','YColor','k', 'LineWidth', 0.5,...
+           'XAxisLocation','top', 'XTick', [],... 
+           'YAxisLocation','right', 'YTick', []);
+linkaxes([ax1, ax2])
